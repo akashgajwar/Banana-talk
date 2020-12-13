f511bfe8e97a1f6313a89b81b8731ea6e21f60a1
akashgajwar <75195665+akashgajwar@users.noreply.github.com>
2020-12-14 02:41:53 +0530

    Error identified

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(input){
+function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
 
@@ -17,14 +17,13 @@ function clickHandler() {
     var inputText = txtInput.value;
 
     fetch(getTranslationURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
 
-    })
-    .catch(errorHandler)
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
-
